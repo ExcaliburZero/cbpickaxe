4bb205c761812e605f5a344e416be9678cf4bd71
Christopher Wells <cwellsny@nycap.rr.com>
2023-10-05 23:10:21 -0700

    Have max move slots script index by en monster name
    Since bestiary index is no longer unique as of PC version 1.5.

--- old_scripts/monster_max_move_slots.py
+++ old_scripts/monster_max_move_slots.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 import argparse
+import pathlib
 import sys
 
 import cbpickaxe as cbp
@@ -12,20 +13,31 @@ FAILURE = 1
 def main(argv: List[str]) -> int:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("monster_form_tres_files", nargs="+")
+    parser.add_argument("--roots", nargs="+", required=True)
+    parser.add_argument(
+        "--monster_form_paths",
+        nargs="+",
+        default=["res://data/monster_forms/", "res://data/monster_forms_secret/"],
+    )
 
     args = parser.parse_args(argv)
 
-    monster_forms: Dict[int, cbp.MonsterForm] = {}
-    for input_file in args.monster_form_tres_files:
-        with open(input_file, "r") as input_stream:
-            monster_form = cbp.MonsterForm.from_tres(input_stream)
+    hoylake = cbp.Hoylake()
+    for root in args.roots:
+        hoylake.load_root(pathlib.Path(root))
 
-        monster_forms[monster_form.bestiary_index] = monster_form
+    monster_forms = {}
+    for monster_forms_path in args.monster_form_paths:
+        monster_forms.update(hoylake.load_monster_forms(monster_forms_path))
 
     print("{{#switch: {{{1|}}}")
-    for _, monster_form in sorted(monster_forms.items()):
-        print(f"| {monster_form.bestiary_index} = {monster_form.max_move_slots}")
+    for _, monster_form in sorted(
+        monster_forms.items(),
+        key=lambda d: (d[1].bestiary_index, hoylake.translate(d[1].name)),
+    ):
+        print(
+            f"| {hoylake.translate(monster_form.name)} = {monster_form.max_move_slots}"
+        )
     print("| #default = Unknown")
     print("}}")
 
