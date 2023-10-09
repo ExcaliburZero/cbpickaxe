41c0f9fe70700b1e62a823bb6afe90e7cefc8c03
Christopher Wells <cwellsny@nycap.rr.com>
2023-10-08 21:55:08 -0700

    Add more docs new regression tests

--- regression_tests/test_regression_generate_docs_new.py
+++ regression_tests/test_regression_generate_docs_new.py
@@ -14,6 +14,36 @@ class TestRegressionGenerateDocsNew(Util.TestRegression):
     stdin = ["Traffikrabdos", "y", "n", "../my_cassette_beasts"]
 
 
+class TestRegressionGenerateDocsYesToEverything(Util.TestRegression):
+    name = "generate_docs_new_yes_to_everything"
+    command = [
+        "cbpickaxe_generate_docs",
+        "new",
+    ]
+    expected_files = ["docs.toml"]
+    stdin = ["Traffikrabdos", "y", "y", "../my_cassette_beasts"]
+
+
+class TestRegressionGenerateDocsNoToEverything(Util.TestRegression):
+    name = "generate_docs_new_no_to_everything"
+    command = [
+        "cbpickaxe_generate_docs",
+        "new",
+    ]
+    expected_files = ["docs.toml"]
+    stdin = ["Traffikrabdos", "n", "n", "../my_cassette_beasts"]
+
+
+class TestRegressionGenerateDocsSpacesInName(Util.TestRegression):
+    name = "generate_docs_new_spaces_in_name"
+    command = [
+        "cbpickaxe_generate_docs",
+        "new",
+    ]
+    expected_files = ["docs.toml"]
+    stdin = ["Traffikrabdos is Cool", "n", "n", "../my_cassette_beasts"]
+
+
 if __name__ == "__main__":
     tests = []
     for name, obj in inspect.getmembers(sys.modules[__name__]):
