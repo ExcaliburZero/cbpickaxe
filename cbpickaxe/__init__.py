4a0fb881397e521179ed4a60d59cb355e8bb4b4b
Christopher Wells <cwellsny@nycap.rr.com>
2023-10-17 19:15:12 -0700

    Export Color class

--- cbpickaxe/__init__.py
+++ cbpickaxe/__init__.py
@@ -4,6 +4,7 @@ A library for data mining the game Cassette Beasts.
 from .animation import Animation, Frame, FrameTag
 from .hoylake import Hoylake
 from .item import Item
+from .misc_types import Color
 from .monster_form import Evolution, MonsterForm, TapeUpgrade
 from .move import Move
 from .translation_table import TranslationTable
@@ -14,6 +15,7 @@ __all__ = [
     "FrameTag",
     "Hoylake",
     "Item",
+    "Color",
     "Evolution",
     "MonsterForm",
     "TapeUpgrade",
