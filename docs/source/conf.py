754ab1762c81c7c08509b0fd7475d741cf48d78c
Christopher Wells <cwellsny@nycap.rr.com>
2023-10-22 19:23:31 -0700

    Update version to v0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "cbpickaxe"
 copyright = "2023, ExcaliburZero"
 author = "ExcaliburZero"
-release = "0.0.1"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
