b1aac2d171df7ceceb7e14065037ad883ecf1a90
Christopher Wells <cwellsny@nycap.rr.com>
2023-11-04 11:23:40 -0700

    Add EN name and max move slots to monster mining script

--- misc_scripts/mine_monster_forms.py
+++ misc_scripts/mine_monster_forms.py
@@ -15,9 +15,14 @@ FAILURE = 1
 def main(argv: List[str]) -> int:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("monster_form_tres_files", nargs="+")
+    parser.add_argument("--roots", nargs="+", required=True)
+    parser.add_argument("--monster_form_tres_files", nargs="+", required=True)
 
     args = parser.parse_args(argv)
+
+    hoylake = cbp.Hoylake()
+    for i, root in enumerate(args.roots):
+        hoylake.load_root(str(i), pathlib.Path(root))
 
     monster_forms: List[Tuple[int, cbp.MonsterForm]] = []
     for input_file in args.monster_form_tres_files:
@@ -29,7 +34,11 @@ def main(argv: List[str]) -> int:
     with open(pathlib.Path("data") / "monster_forms.json", "w") as output_stream:
         data = []
         for _, monster_form in sorted(monster_forms, key=lambda e: (e[0], e[1].name)):
-            data.append(dataclasses.asdict(monster_form))
+            monster_form_dict = dataclasses.asdict(monster_form)
+            monster_form_dict["name_en"] = hoylake.translate(monster_form.name)
+            monster_form_dict["max_move_slots"] = monster_form.max_move_slots
+
+            data.append(monster_form_dict)
 
         json.dump(data, output_stream, indent=4)
 
